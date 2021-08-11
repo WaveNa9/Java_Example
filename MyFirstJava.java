c2ce5992ff5e4352872706bf9d25273e3c9f2b0b
WaveNa <624230016@webmail.npru.ac.th>
2021-08-11 10:40:14 +0700

    Updata data 1

--- MyFirstJava.java
+++ MyFirstJava.java
@@ -1,5 +1,9 @@
 public class MyFirstJava{
     public static void main(String[] args) {
-        System.out.println("My first Java Program");
+        
+        System.out.println("Update data");
+        System.out.println("Git Command");
+
+
     }
 }
